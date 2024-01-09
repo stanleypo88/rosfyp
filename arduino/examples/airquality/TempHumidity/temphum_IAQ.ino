766b4c14dffcda943281da77bd8e0e0107151873
stanleypo88 <40240896+stanleypo88@users.noreply.github.com>
2024-01-10 02:33:13 +0800

    Create temphum_IAQ.ino

---
+++ arduino/examples/airquality/TempHumidity/temphum_IAQ.ino
@@ -0,0 +1,103 @@
+/*
+       file DFRobot_BME680_I2C.ino
+
+       @ https://github.com/DFRobot/DFRobot_BME680
+
+       connect bme680 I2C interface with your board (please reference board compatibility)
+
+       Temprature, Humidity, pressure, altitude, calibrated altitude, gas resistance and IAQ data will be printed via serial.
+
+       Copyright   [DFRobot](http://www.dfrobot.com), 2016
+       Copyright   GNU Lesser General Public License
+
+       version  V1.0
+       date  2017-12-7
+*/
+
+#include "DFRobot_BME680_I2C.h"
+#include "Wire.h"
+
+/*use an accurate altitude to calibrate sea level air pressure*/
+#define CALIBRATE_PRESSURE
+
+DFRobot_BME680_I2C bme(0x76);  //0x77 I2C address
+
+
+float seaLevel;
+void setup()
+{
+  uint8_t       rslt = 1;
+  Serial.begin(115200);
+  while (!Serial);
+  delay(1000);
+  Serial.println();
+  while (rslt != 0) {
+    rslt = bme.begin();
+    if (rslt != 0) {
+      Serial.println("bme begin failure");
+      delay(2000);
+    }
+  }
+  Serial.println("bme begin successful");
+  bme.supportIAQ();
+}
+
+void loop()
+{
+  static uint8_t       calibrated = 0;
+
+#ifdef CALIBRATE_PRESSURE
+  if (calibrated == 0) {
+    if (bme.iaqUpdate() == 0) {
+      /*You can use an accurate altitude to calibrate sea level air pressure.
+        And then use this calibrated sea level pressure as a reference to obtain the calibrated altitude.
+        In this case,525.0m is chendu accurate altitude.
+      */
+      seaLevel = bme.readSeaLevel(525.0);
+      Serial.print("seaLevel :");
+      Serial.println(seaLevel);
+      calibrated = 1;
+    }
+  }
+#else
+  calibrated = 1;
+#endif
+
+  if (calibrated) {
+    uint8_t rslt = bme.iaqUpdate();
+    if (rslt == 0) {
+      Serial.println();
+      Serial.print("timestamp(ms) :");
+      Serial.println(millis());
+      Serial.print("temperature(C) :");
+      Serial.println(bme.readTemperature(), 2);
+      Serial.print("pressure(Pa) :");
+      Serial.println(bme.readPressure());
+      Serial.print("humidity(%rh) :");
+      Serial.println(bme.readHumidity(), 2);
+      Serial.print("altitude(m) :");
+      Serial.println(bme.readAltitude());
+#ifdef CALIBRATE_PRESSURE
+      Serial.print("calibrated altitude(m) :");
+      Serial.println(bme.readCalibratedAltitude(seaLevel));
+#endif
+      Serial.print("gas resistance :");
+      Serial.println(bme.readGasResistance());
+      if (bme.isIAQReady()) {
+        Serial.print("IAQ :");
+        float iaq = bme.readIAQ();
+        Serial.print(iaq);
+        if (iaq < 50) Serial.println(" good");
+        else if (iaq < 100) Serial.println(" average");
+        else if (iaq < 150) Serial.println(" little bad");
+        else if (iaq < 200) Serial.println(" bad");
+        else if (iaq < 300) Serial.println(" worse");
+        else Serial.println(" very bad");
+      } else {
+        Serial.print("IAQ not ready, please wait about ");
+        Serial.print((int)(305000 - millis()) / 1000);
+        Serial.println(" seconds");
+      }
+    }
+  }
+}
